37de2f6520b9f9483d4733e22783b17933cae531
huilling110 <huiling2016@outlook.com>
2021-08-10 22:52:13 +0800

    commented out treeVars related lines

--- BESTAnalyzer/plugins/BESTProducer_v2.cc
+++ BESTAnalyzer/plugins/BESTProducer_v2.cc
@@ -123,7 +123,7 @@ private:
   // TTree *jetTree;
   // TH1F *GenWeightTotal;
   // TH1F *Cutflow;
-  std::map<std::string, float> treeVars;
+  // std::map<std::string, float> treeVars;
   std::vector<std::string> listOfVars;
   // std::map<std::string, std::vector<float> > treeVecVars;
   std::map<std::string, std::vector<int> > intVecVars;
@@ -178,11 +178,6 @@ BESTProducer_v2::BESTProducer_v2(const edm::ParameterSet& iConfig):
 
 
   //now do what ever initialization is needed
-  //???not sure why the output file has a dir "run"?
-  // edm::Service<TFileService> fs;
-  // jetTree = fs->make<TTree>("jetTree","jetTree");
-  // GenWeightTotal = fs->make<TH1F>("GenWeightTotal", "GenWeightTotal", 1, 0.5, 1.5);
-  // Cutflow = fs->make<TH1F>("Cutflow", "Cutflow", 7, 0, 6);
 
   //Store the BEST variables for each jet
   listOfVars.push_back("nJets");//std::vector<std::string> listOfVars
@@ -344,10 +339,10 @@ BESTProducer_v2::BESTProducer_v2(const edm::ParameterSet& iConfig):
   //Signal MC Info
   listOfVars.push_back("VLQDecayMode");
   listOfIntVecVars.push_back("JetGenID");
-  for (unsigned i = 0; i < listOfVars.size(); i++){
-    treeVars[ listOfVars[i] ] = -999.99;
+  // for (unsigned i = 0; i < listOfVars.size(); i++){
+    // treeVars[ listOfVars[i] ] = -999.99;
     // jetTree->Branch( (listOfVars[i]).c_str() , &(treeVars[ listOfVars[i] ]), (listOfVars[i]+"/F").c_str() );
-  }
+  // }
 
   listOfBESTVars_ = {"jetAK8_pt", "jetAK8_mass", "jetAK8_SoftDropMass", "nSecondaryVertices", "bDisc", "bDisc1", "bDisc2", "jetAK8_Tau4", "jetAK8_Tau3", "jetAK8_Tau2", "jetAK8_Tau1", "jetAK8_Tau32", "jetAK8_Tau21", "FoxWolfH1_Higgs", "FoxWolfH2_Higgs", "FoxWolfH3_Higgs", "FoxWolfH4_Higgs", "FoxWolfH1_Top", "FoxWolfH2_Top", "FoxWolfH3_Top", "FoxWolfH4_Top", "FoxWolfH1_W", "FoxWolfH2_W", "FoxWolfH3_W", "FoxWolfH4_W", "FoxWolfH1_Z", "FoxWolfH2_Z", "FoxWolfH3_Z", "FoxWolfH4_Z", "isotropy_Higgs", "sphericity_Higgs", "aplanarity_Higgs", "thrust_Higgs", "sphericity_Top", "aplanarity_Top", "thrust_Top", "sphericity_W", "aplanarity_W", "thrust_W", "sphericity_Z", "aplanarity_Z", "thrust_Z", "nSubjets_Higgs", "nSubjets_Top", "nSubjets_W", "nSubjets_Z", "subjet12_mass_Higgs", "subjet23_mass_Higgs", "subjet13_mass_Higgs", "subjet1234_mass_Higgs", "subjet12_mass_Top", "subjet23_mass_Top", "subjet13_mass_Top", "subjet1234_mass_Top", "subjet12_mass_W", "subjet23_mass_W", "subjet13_mass_W", "subjet1234_mass_W", "subjet12_mass_Z", "subjet23_mass_Z", "subjet13_mass_Z", "subjet1234_mass_Z", "subjet12_CosTheta_Higgs", "subjet23_CosTheta_Higgs", "subjet13_CosTheta_Higgs", "subjet1234_CosTheta_Higgs", "subjet12_CosTheta_Top", "subjet23_CosTheta_Top", "subjet13_CosTheta_Top", "subjet1234_CosTheta_Top", "subjet12_CosTheta_W", "subjet23_CosTheta_W", "subjet13_CosTheta_W", "subjet1234_CosTheta_W", "subjet12_CosTheta_Z", "subjet23_CosTheta_Z", "subjet13_CosTheta_Z", "subjet1234_CosTheta_Z", "subjet12_DeltaCosTheta_Higgs", "subjet13_DeltaCosTheta_Higgs", "subjet23_DeltaCosTheta_Higgs", "subjet12_DeltaCosTheta_Top", "subjet13_DeltaCosTheta_Top", "subjet23_DeltaCosTheta_Top", "subjet12_DeltaCosTheta_W", "subjet13_DeltaCosTheta_W", "subjet23_DeltaCosTheta_W", "subjet12_DeltaCosTheta_Z", "subjet13_DeltaCosTheta_Z","subjet23_DeltaCosTheta_Z", "asymmetry_Higgs", "asymmetry_Top", "asymmetry_W", "asymmetry_Z"};
   //------------------------------------------------------------------------------
